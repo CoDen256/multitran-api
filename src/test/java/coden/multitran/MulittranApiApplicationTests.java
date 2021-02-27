26bb277ae53a06ad0ba3f1f70c2573d5144fb60b
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2021-02-27 22:36:43 +0100

    Initial comit

---
+++ src/test/java/coden/multitran/MulittranApiApplicationTests.java
@@ -0,0 +1,13 @@
+package coden.multitran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulittranApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
