8b114ee978571747c3849c00b8d7a6774439c010
Denys Chernyshov <den.blackshov@gmail.com>
2021-03-02 22:00:38 +0100

    Add bean for multitran client

--- src/main/java/coden/multitran/config/MultitranConfig.java
+++ src/main/java/coden/multitran/config/MultitranConfig.java
@@ -1,9 +1,10 @@
 package coden.multitran.config;
 
+import coden.multitran.crawler.MultitranCrawler;
 import coden.multitran.crawler.MultitranDocumentFetcher;
+import coden.multitran.translation.MultitranTranslationClient;
 import coden.multitran.website.MultitranUrls;
 import org.jsoup.Jsoup;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +12,13 @@ import org.springframework.context.annotation.Configuration;
 public class MultitranConfig {
 
     @Bean
-    @ConditionalOnMissingBean
     MultitranDocumentFetcher documentFetcher(){
         return (s, t, p) -> Jsoup.connect(MultitranUrls.getTranslationUrl(s, t, p)).get();
+    }
+
+    @Bean
+    MultitranTranslationClient multitranClient(MultitranDocumentFetcher fetcher){
+        return new MultitranCrawler(fetcher);
     }
 
 }
